b4488167b68690ba118fb846b6a4474c6771eccd
anpen <aqua.7505@gmail.com>
2025-10-29 21:52:04 +0900

    Safe deploy from XAMPP - 20251029_215158

--- config.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-$host = "mysql748.db.sakura.ne.jp";    // ホスト名
-$user = "purplelion51";      // ユーザー名
-$password = "-6r_am73";  // パスワード
-$dbname = "purplelion51_sorciere";    // データベース名
-?>
